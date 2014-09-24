6428f1c9547556e13096e364c62b45caab25caca
molst <marcus.holst@intelliplan.se>
2014-09-24 11:37:39 +0200

    removed warning, set repl port

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [riemann-clojure-client "0.2.11"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]])
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 [org.slf4j/slf4j-log4j12 "1.6.4"]]
+  :repl-options {:port 6667})
