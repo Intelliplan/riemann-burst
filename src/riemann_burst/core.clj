348625ec4ab9c3e019eb856dd8f7724ee63e3096
molst <marcus.holst@intelliplan.se>
2014-09-24 16:12:13 +0200

    better naming

--- src/riemann_burst/core.clj
+++ src/riemann_burst/core.clj
@@ -9,14 +9,14 @@
        (<! (timeout delay))
        (riemann/send-event riemann-client e)))))
 
-(defn events [template-event metrics] (map #(assoc template-event :metric %) metrics))
+(defn apply-metric-series [template-event metrics] (map #(assoc template-event :metric %) metrics))
 
 (def cycle-1-10
-  (events {:service "debug-metric-series" :description "cycling metric 1..10"}
+  (apply-metric-series {:service "debug-metric-series" :description "cycling metric 1..10"}
           (cycle [0 1 2 3 4 5 6 7 8 9])))
 
 (def spike-every-100
-  (events {:service "debug-metric-series" :description "spike every 100"}
+  (apply-metric-series {:service "debug-metric-series" :description "spike every 100"}
           (cycle (conj (take 99 (cycle [0 1 2 3])) 9))))
 
 (def strange-message-every-100
@@ -26,7 +26,9 @@
 
 ;;function calls below will be evaluated when this file is loaded (using C-c C-k in emacs/CIDER)
 
-(burst! 100 (take 20 cycle-1-10))
+(burst! 100 (apply-metric-series {:service "debug-metric-series" :description "hello!"} [1 2 3 4 5]))
+
+#_(burst! 100 (take 20 cycle-1-10))
 
 #_(burst! 10 (take 200 spike-every-100))
 
